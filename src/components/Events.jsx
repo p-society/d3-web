e84761ee92ad1123c91507e3e24de848ffb38804
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-09-29 18:14:12 +0530

    structured codes and added basic landing page as per design

---
+++ src/components/Events.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Events() {
+  return (
+    <section id="events">
+      <h2>Upcoming Events</h2>
+      <p>Details of upcoming events will be listed here.</p>
+    </section>
+  );
+}
+
+export default Events;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 D3. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
