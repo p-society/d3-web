a13e68cc5971a8e1fc8976e2098631582ff2cfb8
zakhaev26 <b422056@iiit-bh.ac.in>
2024-10-03 22:43:38 +0530

    feat: staging codebase

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,73 +2,73 @@ import React from 'react';
 import '../assets/css/EventRegistration.css'
 import EventDetails from './EventComponent/EventDetails';
 function Register() {
-  const Events=[
+  const Events = [
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     },
     {
-      Event:'LOREM IPSUM',
-      Details:'DETAILS',
-      Register:'Link'
+      Event: 'LOREM IPSUM',
+      Details: 'DETAILS',
+      Register: 'Link'
     }
   ]
   return (
     <section id="Register">
       <div className='EventHeading'>
-      <p className='RFEHeading'>REGISTER FOR EVENTS</p>
-      <p className='ELHeading'>EVENT LIST</p>
+        <p className='RFEHeading'>REGISTER FOR EVENTS</p>
+        <p className='ELHeading'>EVENT LIST</p>
       </div>
-      {Events.map((e)=>(
-          <EventDetails props={e}/>
+      {Events.map((e) => (
+        <EventDetails props={e} />
       ))}
-      
+
     </section>
   );
 }
--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -2,42 +2,34 @@ import React from 'react';
 import SponsorCard from './SponsorCard.jsx';
 import '../assets/css/sponsors.css';
 function Sponsor() {
-  return (
+    return (
         <div className="sponsor-container">
             <h1 className="sponsor-title">Presenting our Sponsors</h1>
             <div className="sponsor-card-container">
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor1.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor2.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor3.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor4.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor1.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor2.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor3.png"
-            />
-            <SponsorCard
-                title="Sponsor Title"
-                image="src/assets/images/sponsor/sponsor4.png"
-            />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
+                <SponsorCard
+                    title="Sponsor Title"
+                    image="src/assets/images/soon.png"
+                />
             </div>
         </div>
     )
