58bbadfbf633c8aa18b8fbe505274fb2b6ad84ab
AvetiLearning/mobile-app-shared-utils <ujsquared>
2024-10-05 23:45:27 +0530

    add co-ordinator, prize-pool, date/time

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -37,51 +37,81 @@ function EventPage() {
             title="CraftNCode 2024"
             image={CraftNCode}
             description="This national-level hackathon invites brilliant minds from across the country to innovate, collaborate, and solve real-world challenges. In the first phase, participating colleges will host hackathons in their respective states. The final round will feature the top teams from this phase, who will earn an invitation to compete at IIIT Bhubaneswar."
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="CyberSec Battle"
             image={CyberSecBattle}
             description="This competition challenges you to address critical issues like financial fraud, security breaches, national security threats, and emerging cyberattacks. The top teams will converge in Bhubaneswar for a final showdown, vying for the prestigious title of CyberSec Battle Champions!"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Code-O-Lympics"
             image={CodeOLympics}
             description="Cod-o-lympics is an ICPC-inspired programming competition where each team competes to solve a set of problems within a given time frame. The team that solves the most problems in the least amount of time will be declared the winner. Participants must form a team of exactly 3 members, and each team must have exactly 2 laptops during the event. The maximum allotted time is 3 hours."
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Pitch and Win"
             image={PitchAndWin}
             description="The Pitch and Win competition challenges participants to create groundbreaking solutions to existing problems, pitch them to industry experts, and make their ideas a reality. Do you have what it takes to impress the panel and walk away with the title of Pitch and Win Champion?"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Tech Talk (Software Edition)"
             image={TechTalkSoftware}
             description="Industry experts and passionate developers converge to share insights on the latest trends, tools, and innovations in software development. Engage in thought-provoking discussions, interactive workshops, and networking opportunities that will inspire your tech journey!"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Tech Talk (Hardware Edition)"
             image={TechTalkHardware}
             description="A dynamic forum where hardware enthusiasts and industry leaders come together to explore cutting-edge technologies, emerging trends, and practical applications in hardware development. Participate in engaging talks, hands-on demonstrations, and collaborative discussions that will elevate your understanding of the hardware landscape!"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Autobot"
             image={Autobot}
             description="This competition challenges you to build and program a fully autonomous robot capable of navigating a complex course filled with sharp turns, unexpected obstacles, and high-speed demands. Whether you’re a seasoned robotics pro or a passionate newcomer, AutoBot offers the perfect platform to compete with fellow enthusiasts and showcase your cutting-edge designs."
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Robo Race"
             image={RoboRace}
             description="Competitors will design and construct robots capable of navigating a specially designed obstacle course that challenges not just speed, but also precision and agility. Your task is to create a robot that can overcome every hurdle and complete the course in the shortest time possible. Every second counts!"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Digicast"
             image={Digicast}
             description="Competitors will design and construct robots capable of navigating a specially designed obstacle course that challenges not just speed, but also precision and agility. Your task is to create a robot that can overcome every hurdle and complete the course in the shortest time possible. Every second counts!"
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
           <EventCard
             title="Build A Bot"
             image={BuildABot}
             description="This event invites students to engage in a thrilling 24-hour challenge where they design and build a functional bot using Arduino or Raspberry Pi. Build a Bot is designed to showcase innovative creations while testing participants' creativity and technical skills."
+            date="date: 20th - 21st march 2024"
+            prize_pool="prize: inr 1,00,000"
+            coordinators="coordinators: ujjwal kala, kala ujjwal "
           />
         </div>
       </div>
