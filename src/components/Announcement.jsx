e1dddea2879db22d5a70776caaee363dd495c563
Abir Banerjee <abirbanerjee793@gmail.com>
2024-10-12 14:48:47 +0530

    announcement banner added

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/announcement.css"
+const Announcement = () => {
+  return (
+    <div className="announcement-bar">
+       Don’t miss out on D3 2024 accommodations! Reserve your spot while availability lasts. 👉
+        <a href="https://forms.gle/qHS2TVTADPBrDiqn6" target="_blank">Book Now</a>
+      </div>
+  )
+}
+
+export default Announcement
