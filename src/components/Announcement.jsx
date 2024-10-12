057898cbd324917602b9148e52b2d1f984d93397
Soubhik Gon <119437069+zakhaev26@users.noreply.github.com>
2024-10-12 15:46:20 +0530

    chore(utf-8): icon fixes

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -3,7 +3,7 @@ import "../assets/css/announcement.css"
 const Announcement = () => {
   return (
     <div className="announcement-bar">
-       Don’t miss out on D³ 2024 accommodations! Reserve your spot while availability lasts. 👉
+       Don’t miss out on D³ 2024 accommodations! Reserve your spot while availability lasts.
         <a href="https://forms.gle/qHS2TVTADPBrDiqn6" target="_blank">Book Now</a>
       </div>
   )
