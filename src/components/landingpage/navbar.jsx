93efe9458c53c442f427fae6e846a2d7416001ca
Anindita Chakraborty <aninditac1912@gmail.com>
2025-09-05 19:26:12 +0530

    navbar made

--- src/components/landingpage/navbar.jsx
+++ src/components/landingpage/navbar.jsx
@@ -1,1 +1,22 @@
-// gitkeep
+
+
+export default function Navbar({ arr }) {
+  return (
+    <div className="w-[1440px] h-[935.238px] bg-[#ffeabb] flex justify-center items-center">
+      <div className="inline-flex p-2 items-center gap-9 rounded-2xl border border-[#F06]">
+        {arr.map((element, index) =>
+         
+            <div
+              key={index}
+              className="text-[#E30F70] font-Cascadia_Code text-[30.476px] 
+                         flex h-10 
+                         px-6 justify-center items-center gap-[10px] hover:text-[#FFEABB] hover:bg-[#F06] hover:rounded-[8px] cursor-pointer"
+            >
+              {element}
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/*/.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        cascadia: ['"Cascadia Code"', 'monospace'],
+      },
+    },
+},
   plugins: [],
 }
+
