a19e62543367aabef19a11d12c27fa831b052a9f
Anindita Chakraborty <aninditac1912@gmail.com>
2025-09-07 00:43:19 +0530

    fixed the placement and margins

--- src/components/landingpage/navbar.jsx
+++ src/components/landingpage/navbar.jsx
@@ -1,16 +1,15 @@
-
 let arr = ["HOME", "EVENTS", "MERCH", "CONTACT US"];
 
 export default function Navbar() {
   return (
-    <div className="w-full bg-[#ffeabb] shadow-md">
-      <div className="flex justify-center items-center gap-9 py-4">
+    <div >
+      <div className="w-[717px]  h-[56px] bg-[#ffeabb]   absolute top-[10px] left-[361px] flex justify-center items-center gap-4 py-2 rounded-2xl border border-[#F06]">
         {arr.map((element, index) => (
           <div
             key={index}
-            className="text-[#E30F70] font-mono text-xl px-6 py-2 
+            className="text-[#E30F70] font-cascadia text-xl px-10 py-1 
                        hover:text-[#FFEABB] hover:bg-[#F06] 
-                       rounded-md cursor-pointer transition"
+                       rounded-md cursor-pointer transition m-1 "
           >
             {element}
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/*/.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        cascadia: ['"Cascadia Code"', 'monospace'],
-      },
-    },
-},
+    extend: {},
+  },
   plugins: [],
 }
-
