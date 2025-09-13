d8c701834f316e73e34f821cbce92e3dbb9d4106
ujsquared <ujsquared@gmail.com>
2025-09-13 23:10:07 +0530

    feat: finally wrap that stuff in a div and make it center.

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -11,38 +11,41 @@ import vectorTop from '../assets/top-vector.svg'
 
 const Landingpage = () => {
   return (
-    <div className="max-h-screen bg-[#ffeabb] w-screen relative overflow-hidden">
-      <div className="w-full flex mt-2 justify-center">
-        <Navbar />
-      </div>
-      <Sidebar />
-      <Grid />
-      <Hero />
+    <div className="min-h-screen bg-[#ffeabb] relative overflow-hidden">
+      {/* Centered container wrapper */}
+      <div className="max-w-screen-2xl mx-auto relative min-h-screen">
+        <div className="w-full flex mt-2 justify-center">
+          <Navbar />
+        </div>
+        <Sidebar />
+        <Grid />
+        <Hero />
 
-      {/* Social Icons - bottom right */}
-      <div className="absolute bottom-8 right-10 z-30 flex gap-6">
-        <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
-        </a>
-        <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
-        </a>
-        <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
-          <FaXTwitter className="text-black w-8 h-8 hover:scale-110 transition" />
-        </a>
-        <a href="https://unstop.com/" target="_blank" rel="noopener noreferrer">
-          <FaLink className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
-        </a>
-      </div>
+        {/* Social Icons - bottom right */}
+        <div className="absolute bottom-8 right-10 z-30 flex gap-6">
+          <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
+          </a>
+          <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
+            <FaXTwitter className="text-black w-8 h-8 hover:scale-110 transition" />
+          </a>
+          <a href="https://unstop.com/" target="_blank" rel="noopener noreferrer">
+            <FaLink className="text-[#000000] w-8 h-8 hover:scale-110 transition" />
+          </a>
+        </div>
 
-      {/* Vector Top in top right corner */}
-      <div className="absolute top-1 right-1 z-30">
-        <img src={vectorTop} alt="Vector Top" className="w-52 h-auto" />
-      </div>
+        {/* Vector Top in top right corner */}
+        <div className="absolute top-1 right-1 z-30">
+          <img src={vectorTop} alt="Vector Top" className="w-52 h-auto" />
+        </div>
 
-      {/* D3 Logo in bottom left */}
-      <div className="absolute top-6/7 left-16 z-30">
-        <img src={d3Logo} alt="D3 Logo" className="w-64" />
+        {/* D3 Logo in bottom left */}
+        <div className="absolute top-6/7 left-16 z-30">
+          <img src={d3Logo} alt="D3 Logo" className="w-64" />
+        </div>
       </div>
       {/* <Event /> */}
     </div>
