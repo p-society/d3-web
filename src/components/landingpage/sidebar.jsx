db10a42d9f17910845ac0919b751ecce2fae9c73
Tushar Anand <tusharannand@gmail.com>
2025-09-23 01:46:26 +0530

    made sizes bigger in landing page
    Signed-off-by: Tushar Anand <tusharannand@gmail.com>

--- src/components/landingpage/sidebar.jsx
+++ src/components/landingpage/sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = ({ isLoaded }) => {
     >
       {/* Vertical bars - pink, lime/yellow, orange with proper spacing from edge */}
       <div className="flex h-full">
-        <div className="w-6 phone:w-8 sm:w-14 h-full bg-[#ff0066] border-2 border-black"></div>
-        <div className="w-6 phone:w-8 sm:w-14 h-full bg-[#c6f806] border-2 border-black"></div>
-        <div className="w-6 phone:w-8 sm:w-14 h-full bg-[#ff8c34] border-2 border-black"></div>
+        <div className="w-6 phone:w-10 sm:w-14 h-full bg-[#ff0066] border-2 border-black"></div>
+        <div className="w-6 phone:w-10 sm:w-14 h-full bg-[#c6f806] border-2 border-black"></div>
+        <div className="w-6 phone:w-10 sm:w-14 h-full bg-[#ff8c34] border-2 border-black"></div>
       </div>
     </div>
   )
