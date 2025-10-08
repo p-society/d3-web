052c6aa46213c6bf0e5e620d21220049e059e719
ujsquared <ujsquared@gmail.com>
2025-10-08 07:53:02 +0530

    fix: fixes for SEO and indexing.
    Signed-off-by: ujsquared <ujsquared@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://d3.techsoc-iiitbbsr.com',
+    }),
+  ],
   server: {
     host: '127.0.0.1',
   },
